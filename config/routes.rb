b829b6e36fed6ba905d136003c03d3c05ef3ab33
IvanPagani <ivanpagani07@gmail.com>
2025-10-27 18:54:33 -0300

    Have the User’s Show page list all the events a user has created

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "events#index"
   resources :events, only: [ :index ]
-
   devise_for :users
+  resources :users, only: [ :show ]
 end
