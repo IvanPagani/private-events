250d9671b46edfd1b0a63b3c55ea522863bc4990
IvanPagani <ivanpagani07@gmail.com>
2025-10-29 18:03:25 -0300

    Separate the past and upcoming events on the Event’s Index page by creating two class methods on the Event model. Refactor the past and upcoming class methods into scopes.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,19 @@
 class Event < ApplicationRecord
+  scope :past,     -> { where("event_datetime < ?",  Time.current).order(event_datetime: :desc) }
+  scope :upcoming, -> { where("event_datetime >= ?", Time.current).order(event_datetime:  :asc) }
+
   validates :title, :location, :event_datetime, presence: true
 
   belongs_to :creator, class_name: "User"
 
   has_many :event_attendances, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :event_attendances, source: :attendee
+
+  # def self.past
+  #  self.where("event_datetime < ?", Time.current).order(event_datetime: :desc)
+  # end
+  #
+  # def self.upcoming
+  #  self.where("event_datetime >= ?", Time.current).order(event_datetime: :asc)
+  # end
 end
